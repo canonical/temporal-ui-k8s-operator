8f43ebfefdefe32f6c8d2bd8999352d2a66d1c3d
nikita-jare <nikitajare2021@gmail.com>
2026-04-16 10:58:43 -0400

    change ubuntu base

--- tests/integration/conftest.py
+++ tests/integration/conftest.py
@@ -72,7 +72,7 @@ def deploy_temporal_stack(
         config={
             "num-history-shards": 1,
         },
-        base="ubuntu@22.04",
+        base="ubuntu@24.04",
     )
 
     juju.deploy(
